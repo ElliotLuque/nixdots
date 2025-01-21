fd7b20d330f07fbb1020c3b9c0856f6a2aafacc5
ElliotLuque <elliotluque@gmail.com>
2025-01-22 00:14:36 +0100

    chore: rewrite comment

--- hosts/nixos-pc/default.nix
+++ hosts/nixos-pc/default.nix
@@ -6,10 +6,10 @@
 
 {
   imports = [
-    ./hardware-configuration.nix
     inputs.home-manager.nixosModules.default
+    ./hardware-configuration.nix
     ../../modules/nixos
-    ../../modules/nixos/nvidia.nix # This is a NVIDIA PC
+    ../../modules/nixos/nvidia.nix # This is a NVIDIA PC so we'll import it
   ];
 
   powerManagement.cpuFreqGovernor = "performance";
