4341b8c9d3ce05e35ae6fb19fdf086cbf6344983
ElliotLuque <elliotluque@gmail.com>
2025-01-22 00:16:29 +0100

    feat: lf, nitch

--- modules/home-manager/packages.nix
+++ modules/home-manager/packages.nix
@@ -4,20 +4,22 @@
     btop
     bat
     eza
+    lf
     tofi
     lazygit
     cmake
     pfetch-rs
     nerdfetch
     fastfetch
+    nitch
     cowsay
     pipes-rs
     cava
     ncspot
     google-chrome
+    zathura
     swww
     noto-fonts-emoji
-    noto-fonts-color-emoji
     jetbrains-mono
     nerd-fonts.jetbrains-mono
   ];
