4723b8e7c6ffee00366a53bd7548f308c8553595
ElliotLuque <elliotluque@gmail.com>
2025-01-22 01:07:42 +0100

    chore: update home default.nix

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,11 +1,13 @@
 { pkgs, lib, inputs, catppuccin, ... }:
 {
   imports = [
+    ./packages.nix
     ./kitty.nix
     ./fish.nix
-    ./starship.nix
     ./catppuccin.nix
+    ./starship.nix
     ./git.nix
+    # ./ncspot.nix
     ./gtk.nix
     ./waybar.nix
     ./hyprland.nix
