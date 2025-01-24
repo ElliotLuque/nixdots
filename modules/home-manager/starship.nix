b52e7bff6c99d9b9bc294249ac6752b6fac0b4ee
ElliotLuque <elliotluque@gmail.com>
2025-01-25 00:59:09 +0100

    feat: prompt nixos icon

--- modules/home-manager/starship.nix
+++ modules/home-manager/starship.nix
@@ -1,7 +1,13 @@
-{ ... }:
 {
   programs.starship = {
     enable = true;
     enableFishIntegration = true;
+
+    settings = {
+      character = {
+        success_symbol = " [](bold blue)  [➜](bold green)";
+        error_symbol = " [](bold blue)  [➜](bold red)";
+      };
+    };
   };
 }
