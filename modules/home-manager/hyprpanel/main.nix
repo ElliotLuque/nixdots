fb31a30366b3f0481b98f07081cebae5cb1ba987
ElliotLuque <elliotluque@gmail.com>
2025-04-19 03:18:35 +0200

    feat: hyprpanel styling

--- modules/home-manager/hyprpanel/main.nix
+++ modules/home-manager/hyprpanel/main.nix
@@ -3,19 +3,36 @@ let
   font = "${config.stylix.fonts.monospace.name}";
   font-size = "${toString config.stylix.fonts.sizes.desktop}";
 
-  workspace-active = "#${config.lib.stylix.colors.base05}";
+  workspace-active = "#${config.lib.stylix.colors.base07}";
   workspace-occupied = "#${config.lib.stylix.colors.base04}";
 
   background = "#${config.lib.stylix.colors.base02}";
   accent = "#${config.lib.stylix.colors.base0E}";
 
+  shutdown-color = "#${config.lib.stylix.colors.base08}";
+  restart-color = "#${config.lib.stylix.colors.base0B}";
+  logout-color = "#${config.lib.stylix.colors.base09}";
+  sleep-color = "#${config.lib.stylix.colors.base0D}";
   dashboard-color = "#${config.lib.stylix.colors.base0D}";
   volume-color = "#${config.lib.stylix.colors.base0B}";
   network-color = "#${config.lib.stylix.colors.base0D}";
   bluetooth-color = "#${config.lib.stylix.colors.base0C}";
   clock-color = "#${config.lib.stylix.colors.base0E}";
-  notifications-color = "#${config.lib.stylix.colors.base0F}";
+  notifications-color = "#${config.lib.stylix.colors.base0A}";
+
   notification-title-color = "#${config.lib.stylix.colors.base0D}";
+
+  cpu-color = "#${config.lib.stylix.colors.base08}";
+  ram-color = "#${config.lib.stylix.colors.base09}";
+  gpu-color = "#${config.lib.stylix.colors.base0B}";
+  disk-color = "#${config.lib.stylix.colors.base0D}";
+
+  directory1-color = "#${config.lib.stylix.colors.base08}";
+  directory2-color = "#${config.lib.stylix.colors.base09}";
+  directory3-color = "#${config.lib.stylix.colors.base0A}";
+  directory4-color = "#${config.lib.stylix.colors.base0B}";
+  directory5-color = "#${config.lib.stylix.colors.base0C}";
+  directory6-color = "#${config.lib.stylix.colors.base0D}";
 in
 {
   programs.hyprpanel = {
@@ -71,12 +88,24 @@ in
         };
 
         dashboard = {
+          powermenu.avatar.image = "/home/elliot/dotfiles/nixdots/modules/home-manager/hyprpanel/profile.png";
           stats.enable_gpu = true;
+
+          shortcuts = {
+            left = {
+              shortcut1 = {
+                command = "google-chrome-stable";
+                tooltip = "Google Chrome";
+                icon = "";
+              };
+            };
+          };
         };
       };
     };
 
     override = {
+      "theme.bar.menus.menu.dashboard.profile.radius" = "999px";
       "theme.bar.border.location" = "bottom";
       "theme.bar.border.color" = "${accent}";
       "theme.bar.opacity" = 85;
@@ -90,6 +119,9 @@ in
       "theme.bar.buttons.notifications.icon" = "${notifications-color}";
       "theme.bar.buttons.dashboard.icon" = "${dashboard-color}";
 
+      "theme.bar.buttons.workspaces.pill.width" = "4em";
+      "theme.bar.buttons.workspaces.pill.active_width" = "18em";
+      "theme.bar.buttons.workspaces.pill.height" = "3.5em";
       "theme.bar.buttons.workspaces.hover" = "${accent}";
       "theme.bar.buttons.workspaces.active" = "${workspace-active}";
       "theme.bar.buttons.workspaces.occupied" = "${workspace-occupied}";
@@ -97,14 +129,32 @@ in
       "theme.bar.buttons.volume.text" = "${volume-color}";
       "theme.bar.buttons.volume.icon" = "${background}";
       "theme.bar.buttons.volume.icon_background" = "${volume-color}";
+      "theme.bar.menus.menu.volume.label.color" = "${volume-color}";
+      "theme.bar.menus.menu.volume.listitems.active" = "${volume-color}";
+      "theme.bar.menus.menu.volume.icons.active" = "${volume-color}";
+      "theme.bar.menus.menu.volume.iconbutton.active" = "${volume-color}";
+      "theme.bar.menus.menu.volume.audio_slider.primary" = "${volume-color}";
+      "theme.bar.menus.menu.volume.input_slider.primary" = "${volume-color}";
 
       "theme.bar.buttons.bluetooth.text" = "${bluetooth-color}";
       "theme.bar.buttons.bluetooth.icon" = "${background}";
       "theme.bar.buttons.bluetooth.icon_background" = "${bluetooth-color}";
+      "theme.bar.menus.menu.bluetooth.label.color" = "${bluetooth-color}";
+      "theme.bar.menus.menu.bluetooth.listitems.active" = "${bluetooth-color}";
+      "theme.bar.menus.menu.bluetooth.icons.active" = "${bluetooth-color}";
+      "theme.bar.menus.menu.bluetooth.iconbutton.active" = "${bluetooth-color}";
+      "theme.bar.menus.menu.bluetooth.scroller.color" = "${bluetooth-color}";
+      "theme.bar.menus.menu.bluetooth.switch.enabled" = "${bluetooth-color}";
 
       "theme.bar.buttons.network.text" = "${network-color}";
       "theme.bar.buttons.network.icon" = "${background}";
       "theme.bar.buttons.network.icon_background" = "${network-color}";
+      "theme.bar.menus.menu.network.label.color" = "${network-color}";
+      "theme.bar.menus.menu.network.listitems.active" = "${network-color}";
+      "theme.bar.menus.menu.network.icons.active" = "${network-color}";
+      "theme.bar.menus.menu.network.iconbuttons.active" = "${network-color}";
+      "theme.bar.menus.menu.network.scroller.color" = "${network-color}";
+      "theme.bar.menus.menu.network.switch.enabled" = "${network-color}";
 
       "theme.bar.buttons.clock.text" = "${clock-color}";
       "theme.bar.buttons.clock.icon" = "${background}";
@@ -120,6 +170,52 @@ in
       "notifications.active_monitor" = false;
       "notifications.monitor" = 1;
       "notifications.clearDelay" = 200;
+      "theme.bar.menus.menu.notifications.label" = "${notifications-color}";
+      "theme.bar.menus.menu.notifications.switch.enabled" = "${notifications-color}";
+      "theme.bar.menus.menu.notifications.scrollbar.color" = "${notifications-color}";
+      "theme.bar.menus.menu.notifications.pager.button" = "${notifications-color}";
+
+      "theme.bar.menus.menu.power.buttons.shutdown.icon_background" = "${shutdown-color}";
+      "theme.bar.menus.menu.power.buttons.shutdown.text" = "${shutdown-color}";
+      "theme.bar.menus.menu.power.buttons.restart.icon_background" = "${restart-color}";
+      "theme.bar.menus.menu.power.buttons.restart.text" = "${restart-color}";
+      "theme.bar.menus.menu.power.buttons.logout.icon_background" = "${logout-color}";
+      "theme.bar.menus.menu.power.buttons.logout.text" = "${logout-color}";
+      "theme.bar.menus.menu.power.buttons.sleep.icon_background" = "${sleep-color}";
+      "theme.bar.menus.menu.power.buttons.sleep.text" = "${sleep-color}";
+
+      "theme.bar.menus.menu.dashboard.powermenu.shutdown" = "${shutdown-color}";
+      "theme.bar.menus.menu.dashboard.powermenu.restart" = "${restart-color}";
+      "theme.bar.menus.menu.dashboard.powermenu.logout" = "${logout-color}";
+      "theme.bar.menus.menu.dashboard.powermenu.sleep" = "${sleep-color}";
+
+      "theme.bar.menus.menu.dashboard.profile.name" = "${accent}";
+
+      "theme.bar.menus.menu.dashboard.controls.wifi.background" = "${network-color}";
+      "theme.bar.menus.menu.dashboard.controls.bluetooth.background" = "${bluetooth-color}";
+      "theme.bar.menus.menu.dashboard.controls.notifications.background" = "${notifications-color}";
+      "theme.bar.menus.menu.dashboard.controls.volume.background" = "${volume-color}";
+      "theme.bar.menus.menu.dashboard.controls.input.background" = "${accent}";
+
+      "theme.bar.menus.menu.dashboard.directories.left.top.color" = "${directory1-color}";
+      "theme.bar.menus.menu.dashboard.directories.left.middle.color" = "${directory2-color}";
+      "theme.bar.menus.menu.dashboard.directories.left.bottom.color" = "${directory3-color}";
+      "theme.bar.menus.menu.dashboard.directories.right.middle.color" = "${directory4-color}";
+      "theme.bar.menus.menu.dashboard.directories.right.top.color" = "${directory5-color}";
+      "theme.bar.menus.menu.dashboard.directories.right.bottom.color" = "${directory6-color}";
+
+      "theme.bar.menus.menu.dashboard.monitors.cpu.icon" = "${cpu-color}";
+      "theme.bar.menus.menu.dashboard.monitors.cpu.bar" = "${cpu-color}";
+      "theme.bar.menus.menu.dashboard.monitors.cpu.label" = "${cpu-color}";
+      "theme.bar.menus.menu.dashboard.monitors.ram.icon" = "${ram-color}";
+      "theme.bar.menus.menu.dashboard.monitors.ram.bar" = "${ram-color}";
+      "theme.bar.menus.menu.dashboard.monitors.ram.label" = "${ram-color}";
+      "theme.bar.menus.menu.dashboard.monitors.gpu.icon" = "${gpu-color}";
+      "theme.bar.menus.menu.dashboard.monitors.gpu.bar" = "${gpu-color}";
+      "theme.bar.menus.menu.dashboard.monitors.gpu.label" = "${gpu-color}";
+      "theme.bar.menus.menu.dashboard.monitors.disk.icon" = "${disk-color}";
+      "theme.bar.menus.menu.dashboard.monitors.disk.bar" = "${disk-color}";
+      "theme.bar.menus.menu.dashboard.monitors.disk.label" = "${disk-color}";
     };
   };
 }
