3c40de5601c900b88f6c956115c8b122e84b48d8
ElliotLuque <elliotluque@gmail.com>
2025-04-29 17:52:12 +0200

    feat: nvidia prime laptop

--- hosts/nixos-laptop/default.nix
+++ hosts/nixos-laptop/default.nix
@@ -11,6 +11,7 @@
     ./hardware-configuration.nix
     ../../modules/nixos
     ../../modules/nixos/nvidia.nix # This is a NVIDIA PC so we'll import it
+    ../../modules/nixos/nvidia-prime.nix # NVIDIA Laptop
   ];
 
   powerManagement.cpuFreqGovernor = "performance";
---
+++ modules/nixos/nvidia-prime.nix
@@ -0,0 +1,8 @@
+{
+  hardware.nvidia.prime = {
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:2:0:0";
+
+    sync.enable = true;
+  };
+}
