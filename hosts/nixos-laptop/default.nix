a07745496d7ad5bdab7e2cb1cb53ef154fb7ea80
ElliotLuque <elliotluque@gmail.com>
2025-04-29 17:30:03 +0200

    fix: hyprland host loading

--- hosts/nixos-laptop/default.nix
+++ hosts/nixos-laptop/default.nix
@@ -6,11 +6,11 @@
 
 {
   imports = [
+    ../../modules/home-manager/hyprland/nixos-laptop.nix
     inputs.home-manager.nixosModules.default
     ./hardware-configuration.nix
     ../../modules/nixos
     ../../modules/nixos/nvidia.nix # This is a NVIDIA PC so we'll import it
-    ../../modules/home-manager/hyprland/nixos-laptop.nix
   ];
 
   powerManagement.cpuFreqGovernor = "performance";
