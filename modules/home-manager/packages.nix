5704d66b5d13347d559e4aa71b9469579da7027f
ElliotLuque <git@elliotluque.com>
2026-02-17 22:14:11 +0100

    chore: update sops key, upv wifi

--- modules/home-manager/packages.nix
+++ modules/home-manager/packages.nix
@@ -49,6 +49,7 @@
     opencode
     pamixer
     brightnessctl
+    whisper-cpp
     noto-fonts-color-emoji
     nerd-fonts.jetbrains-mono
     gnumake
--- modules/home-manager/sops.nix
+++ modules/home-manager/sops.nix
@@ -1,10 +1,7 @@
 { lib, config, ... }:
 {
-  sops = lib.mkIf config.programs.hyprpanel.enable {
+  sops = {
     age.keyFile = "/home/elliot/.config/sops/age/keys.txt";
     defaultSopsFile = ../../secrets/secrets.yaml;
-    secrets."hyprpanel/weather_key" = {
-      path = "%r/weather_key.txt";
-    };
   };
 }
