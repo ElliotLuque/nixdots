8cf698128ee3dae37a017af913c415929e234536
ElliotLuque <git@elliotluque.com>
2026-02-27 18:19:13 +0100

    feat: pnpm alias

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -33,6 +33,7 @@ in
     shellAliases = {
       gg = "lazygit";
       oc = "opencode";
+      pn = "pnpm";
       ls = "eza -lh --icons --group-directories-first";
       lsa = "eza -lah --icons --group-directories-first";
       tree = "eza --tree --icons --group-directories-first";
