ac9c8ebd69b051ac56ce51007535848d684f935d
ElliotLuque <git@elliotluque.com>
2026-09-05 18:11:39 +0200

    feat: app image support

--- modules/nixos/programs.nix
+++ modules/nixos/programs.nix
@@ -4,6 +4,11 @@
     fish.enable = true;
     dconf.enable = true;
 
+    appimage = {
+      enable = true;
+      binfmt = true;
+    };
+
     hyprland = {
       enable = true;
       package = inputs.hyprland.packages."${pkgs.stdenv.hostPlatform.system}".hyprland;
